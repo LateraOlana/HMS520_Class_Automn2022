a9a43bf8dd1a097ed035a399f216533323626585
EPHI-Team <61625870+EPHI-Team@users.noreply.github.com>
2022-10-17 14:43:10 -0700

    Code added to the new R file, I had the code on new repository - dont be alarmed

---
+++ Assignment1.R
@@ -0,0 +1,80 @@
+# Name, place of birth and push ups
+hometown <- "Nekemte"
+my_name <- "Latera T. Olana"
+#On Earth sure, maximum 5 - 5 is even only on a good day. 
+#But I am imagining my self on planet with 0 gravity.
+# Question for the philosophers, would there be push 'ups' in 0 gravity, if
+# nothing is pulling us 'down' -- mind blown
+num_push_ups <- Inf
+print(typeof(num_push_ups))
+need_exercise <- c(num_push_ups <= 5)
+
+#Creating a vector,
+vec1 <- c(1, 2, 3, 4, 5)
+vec2 <- rep(10,100)
+vec3<- rep(vec1,2)
+vec4 <- rep(vec1, each=2)
+vec5 <- seq(0, 1, by=0.01)  
+
+#Work on data rivers
+data("rivers")
+is.vector(rivers) # Returned True, so the data type is vector
+is.list(rivers) # Returned False
+typeof(rivers) # Returned double
+log_rivers <- log10(rivers)
+summary <- c(length = length(rivers), sum = sum(rivers), median = median(rivers), mean = mean(rivers), minimum = min(rivers), maximum = max(rivers), sd = sd(rivers))
+summary
+names(summary) <- c("Length","Summation", "Median","Mean","Minimum","Maximum","Standard Devition")
+summary
+
+#sorted in ascending order
+log_rivers <- sort(log_rivers)
+log_rivers
+
+#Extracting highest and lowest
+n <- length(rivers)-10
+lowest <- log_rivers[c(1:10)]
+lowest
+highest <- log_rivers[-c(1:n)]
+highest
+trimmed_log_rivers <- c(lowest, highest)
+trimmed_log_rivers
+
+#Summary for trimmed summary
+trimmed_summary <- c(length = length(trimmed_log_rivers), sum = sum(trimmed_log_rivers), median = median(trimmed_log_rivers), mean = mean(trimmed_log_rivers), minimum = min(trimmed_log_rivers), maximum = max(trimmed_log_rivers), sd = sd(trimmed_log_rivers))
+trimmed_summary
+names(trimmed_summary) <- c("Length","Summation", "Median","Mean","Minimum","Maximum","Standard Devition")
+trimmed_summary
+
+
+#Creating list and using lists
+info <- list(x = c(5, 6, 7, 8), y = c("a", "b", "c", "d"))
+info$y <- c(1, 2, 3, 4)
+mean_x <- mean(info$x)
+mean_y <- mean(info$y)
+sapply(info, mean)
+
+log_x <- log(info$x)
+log_x <- list(logx = c(log_x))
+info <- append(info, log_x)
+
+distance <- sqrt((info$x)^2 + (info$y)^2)
+distance <- list(dist = c(distance))
+info <- append(info, distance)
+info$logx <- NULL
+info
+
+
+#Manipulating string through vector
+words <- c("brilliant", "joy", "positivity", "strength", "health", "insight")
+words_of_the_day <- paste(words, "is the word of the day!")
+
+#Extracting depending on the first character
+a_h_words <- substring(words,first=1, last = 1)
+a_h_words <- words[a_h_words<= 'h']
+a_h_words
+
+#Extracting depending on the last character
+h_y_words <- substring(words,nchar(words)-1+1, nchar(words))
+h_y_words <- words[h_y_words<= 'y' & h_y_words>= 'h']
+h_y_words
