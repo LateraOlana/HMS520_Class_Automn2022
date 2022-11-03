2ae5282297044b13fc33af0bc986e2de92da2bc3
EPHI-Team <61625870+EPHI-Team@users.noreply.github.com>
2022-11-03 15:41:44 -0700

    Assignment 2: Q3 Answered

--- assignment2/Assignment2.R
+++ assignment2/Assignment2.R
@@ -9,5 +9,16 @@ is_string <- function(x){
   }
 }
 # Call the function
-is_string(c("Star Mort Rickturn of the Jerri"))
+x <- is_string(c("Star Mort Rickturn of the Jerri"))
 
+my_cumsum <- function(x) {
+  if (length(x)==1) return(x)
+  for (i in 2:length(x)) {
+    try(if(!is.numeric(x[i])) stop("x must be a numeric vector"))
+    x[i] <- sum(x[(i - 1):i])
+  }
+  return (x)
+}
+
+x <- my_cumsum(c(3,1,2))
+x
