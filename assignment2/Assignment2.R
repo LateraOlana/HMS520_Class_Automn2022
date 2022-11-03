ae5eb7281bd5878459694513c9e833e339f868e3
EPHI-Team <61625870+EPHI-Team@users.noreply.github.com>
2022-11-03 16:14:12 -0700

    Assignment 2: Q3 Answer added and edited

--- assignment2/Assignment2.R
+++ assignment2/Assignment2.R
@@ -1,4 +1,4 @@
-#Creating function, which returns True if the variable is a string
+#Q1: Creating function, which returns True if the variable is a string
 # False other wise.
 is_string <- function(x){
   if (length(x) == 1){
@@ -10,8 +10,9 @@ is_string <- function(x){
 }
 # Call the function
 x <- is_string(c("Star Mort Rickturn of the Jerri"))
+x
 
-#Function replacing cumsum built-in function
+#Q2: Function replacing cumsum built-in function
 my_cumsum <- function(x) {
   if (length(x)==1) return(x)
   for (i in 2:length(x)) {
@@ -26,6 +27,28 @@ my_cumsum <- function(x) {
   return (x)
 }
 
-#Call my_cumsum function()
+#Call my_cumsum function
 x <- my_cumsum(c(3,'d',2))
 x
+
+#Q3: Creating RMSE function
+rmse <- function(x,na_rm){
+  if ((is.numeric(x) != TRUE)){
+    return("x must be a numeric vector")
+  }
+  else{
+    if (na_rm == TRUE){
+      x <- c(na.omit(x))
+    }
+    else{
+      #I will replace it by zero as as no alternative is given in the assignment
+      x[is.na(x)] <- 0
+    }
+    return(sqrt((sum(x^2))/length(x)))
+  }
+}
+x<-rmse(c(1,2,3,'NA'),FALSE)
+x
+
+
+    
